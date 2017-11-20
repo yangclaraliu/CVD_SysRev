ff9223811f82b932331107d7e1d38a9cb30df924
Yang Liu <yang.clara.liu@gmail.com>
2017-11-19 18:45:57 -0600

    RedCap_Template

--- RedCap_Template/gen.temp.R
+++ RedCap_Template/gen.temp.R
@@ -3,7 +3,6 @@
 #' @param no_estimates captures the number of estimates
 #' @initial should be your initial as a character string
 
-
 gen.temp <- function(PMID,no_estimates,initial){
   if(!require(RISmed)) install.packages("RISmed")
   if(grepl('^[A-Za-z]+$', PMID)) stop("This PubMed ID does not look right. :(\n")
@@ -11,10 +10,11 @@ gen.temp <- function(PMID,no_estimates,initial){
   cat(paste("Please check if the title matches with what you are working with:\n",ArticleTitle(EUtilsGet(PMID)),"\n"))
   if(no_estimates<=20) stop("Too Few Effect Estimates to Access this Template. :(\n")
   temp_head <- read.csv("RedCap_Template.csv")
+  temp_head <- temp_head[-1,]
   temp_head[,paste("ee",1:no_estimates,sep="")] <- data.frame(matrix(nrow=nrow(temp_head),ncol=no_estimates))
   file_name <- paste(PMID,"_",initial,".csv",sep="")
   cat(paste("\nYour template named ", file_name, "has been saved to ",getwd(),".",sep=""))
-  write.csv(temp_head,file = file_name)
+  write.csv(temp_head,file = file_name, row.names = FALSE)
 }
 
 
--- RedCap_Template/run.me.R
+++ RedCap_Template/run.me.R
@@ -1,2 +1,9 @@
+#' this is the function used to generate the template for RedCap entries with more than 20 effect estimates
+#' @param PMID is the PubMed ID of the paper that you are working with, it sould be a number
+#' @param no_estimates captures the number of estimates
+#' @initial should be your initial as a character string
+
 source("gen.temp.R")
+
+#example
 gen.temp(9366732,21,"YK")
